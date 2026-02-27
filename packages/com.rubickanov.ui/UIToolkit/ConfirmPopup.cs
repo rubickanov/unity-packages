c029c180bf67bce4172686481f5963d76db5e695
rubickanov <rubickanov@gmail.com>
2026-02-27 21:41:03 +0400

    Fix ui toolkit package dependencies

--- packages/com.rubickanov.ui/UIToolkit/ConfirmPopup.cs
+++ packages/com.rubickanov.ui/UIToolkit/ConfirmPopup.cs
@@ -1,20 +1,10 @@
 using Cysharp.Threading.Tasks;
-using Rubickanov.UI.Animations;
 using UnityEngine.UIElements;
 
 namespace Rubickanov.UI.UIToolkit
 {
     public class ConfirmPopup : UIToolkitView<ConfirmViewModel>
     {
-        private UIToolkitAnimationTarget _overlay = default!;
-        private UIToolkitAnimationTarget _panel = default!;
-
-        protected override void OnInitialize()
-        {
-            _overlay = new UIToolkitAnimationTarget(Root.Q(className: "overlay"));
-            _panel = new UIToolkitAnimationTarget(Root.Q(className: "panel"));
-        }
-
         protected override UniTask OnBind()
         {
             Root.Q<Label>("title-label").text = ViewModel.Title;
@@ -36,20 +26,6 @@ namespace Rubickanov.UI.UIToolkit
             Root.Focus();
 
             return UniTask.CompletedTask;
-        }
-
-        protected override async UniTask OnShowAsync(IAnimationTarget root, float duration)
-        {
-            await UniTask.WhenAll(
-                ViewAnimations.Fade.PlayShowAsync(_overlay, duration),
-                ViewAnimations.FadeAndScale.PlayShowAsync(_panel, duration));
-        }
-
-        protected override async UniTask OnHideAsync(IAnimationTarget root, float duration)
-        {
-            await UniTask.WhenAll(
-                ViewAnimations.Fade.PlayHideAsync(_overlay, duration),
-                ViewAnimations.FadeAndScale.PlayHideAsync(_panel, duration));
         }
 
         private void OnNavigationCancel(NavigationCancelEvent evt)
